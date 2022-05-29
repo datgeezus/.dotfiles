1fbe1df930ae5b8bc26f3849da65d16ff81bb945
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-05-29 13:01:44 -0400

    lua config

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,26 @@
+local options = {
+    backup = false
+    writebackup = false
+    clipboard = "unnamedplus"   -- allow nvim access the system clipboard
+    mouse = "a"         -- allow the mouse to be used in nvim
+    tabstop = 4         -- number of visual spaces per tab
+    softtabstop = 4     -- number of spaces in tab when editting
+    shiftwidth = 4
+    expandtab = true    -- tabs as spaces
+    splitbeloww = true
+    splitright = true
+    number = ture       -- show line numbers
+    fileencoding = "utf-8"
+    showmatch = true    -- highlight matchin [{()}]
+    cursorline = true   -- highlight current line
+    incsearch = true    -- search as charactes are entered
+    hlsearch = true     -- hightlight search matches
+    ignorecase = true   -- ignore case in searches
+    smartcase = true
+    swapfile = false,   -- creates a swap file
+}
+
+
+for k,v in pairs(options) do
+    vim.opt[k] = v
+end
