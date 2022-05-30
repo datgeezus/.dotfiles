dce258c7a9b529a8363a6a251ec390721d2f9bcc
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-05-29 22:05:30 -0400

    cleanup

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -6,8 +6,6 @@ Plug 'nvim-lua/plenary.nvim'
 Plug 'airblade/vim-gitgutter'
 Plug 'Yggdroot/indentLine'
 Plug 'nvim-lualine/lualine.nvim'
--- Plug 'itchyny/lightline.vim'
--- Plug 'scrooloose/syntastic'
 Plug 'tpope/vim-commentary'
 Plug 'tpope/vim-fugitive'
 Plug 'tpope/vim-repeat'
@@ -15,9 +13,6 @@ Plug 'tpope/vim-sensible'
 Plug 'tpope/vim-surround'
 Plug 'tpope/vim-unimpaired'
 Plug 'tpope/vim-vinegar'
--- Plug 'sheerun/vim-polyglot'
--- Plug('junegunn/fzf', {['do'] = vim.fn['fzf#install']})
--- Plug 'junegunn/fzf.vim'
 Plug 'sainnhe/gruvbox-material'
 Plug('EdenEast/nightfox.nvim', {tag = 'v1.0.0'})
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = vim.fn[':TSUpdate']})
