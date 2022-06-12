6348a3a63d78831edd7a67869cd3e81c817b4b4a
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-06-12 16:07:57 -0400

    custom awesome theme

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -45,11 +45,11 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(gears.filesystem.get_configuration_dir() .. "theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "kitty"
-editor = os.getenv("EDITOR") or "nano"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
