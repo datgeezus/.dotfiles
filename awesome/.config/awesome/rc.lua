55787b69f22ed99d738a7b4e17dc4f7e3c049038
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-06-12 14:24:25 -0400

    cleanup

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -311,11 +311,7 @@ globalkeys = gears.table.join(
         {description = "restore minimized", group = "client"}),
 
     -- Prompt
-    awful.key({ modkey }, "space",
-        function ()
-            -- awful.screen.focused().mypromptbox:run()
-            awful.util.spawn("dmenu_run")
-        end,
+    awful.key({ modkey }, "space", function () awful.util.spawn("dmenu_run") end,
         {description = "run dmenu", group = "launcher"}),
 
     awful.key({ modkey }, "x",
@@ -340,7 +336,7 @@ clientkeys = gears.table.join(
             c:raise()
         end,
         {description = "toggle fullscreen", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
+    awful.key({ modkey }, "q",      function (c) c:kill()                         end,
         {description = "close", group = "client"}),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
         {description = "toggle floating", group = "client"}),
