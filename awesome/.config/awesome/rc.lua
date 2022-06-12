e1aafadc3d1a50a65d1c52571211d5fc96f421e3
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-06-11 20:30:46 -0400

    gaps

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -566,3 +566,6 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- Gaps
+beautiful.useless_gap = 5
