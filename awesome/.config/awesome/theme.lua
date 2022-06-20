ba6fd16a72893b5098ceafb686b923892b231609
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-06-20 11:41:31 -0400

    font size

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -11,7 +11,7 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font          = "TerminessTTF Nerd Font 10.5"
+theme.font          = "TerminessTTF Nerd Font 11"
 
 theme.bg_normal     = "#222222"
 theme.bg_focus      = "#535d6c"
