a3eefe0989092fa9389f07271a4ec2309cd28aea
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-06-20 13:51:22 -0400

    remove keyboard layout

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -103,7 +103,7 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 -- }}}
 
 -- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
+-- mykeyboardlayout = awful.widget.keyboardlayout()
 
 -- {{{ Wibar
 -- Create a textclock widget
@@ -210,7 +210,6 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
             s.mylayoutbox,
