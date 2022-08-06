137124b2d0912019db5c676b4228b477c6af59a5
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-08-06 14:42:55 -0400

    lsp keymaps

--- nvim/.config/nvim/lua/lsp/configs.lua
+++ nvim/.config/nvim/lua/lsp/configs.lua
@@ -9,6 +9,11 @@ mason_lspconfig.setup({
 local function keymaps()
     vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
+    vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
+    vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
+    vim.keymap.set("n", "<leader>dl", "<CMD>Telescope diagnostics<CR>", { buffer = 0 })
 end
 
 
