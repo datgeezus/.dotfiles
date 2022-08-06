c7d347939088665096833e09f5edcde2937b9c07
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-08-06 14:42:55 -0400

    lsp keymaps

--- nvim/.config/nvim/lua/lsp/configs.lua
+++ nvim/.config/nvim/lua/lsp/configs.lua
@@ -9,8 +9,13 @@ mason_lspconfig.setup({
 local function keymaps()
     vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
-    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
+    vim.keymap.set("n", "gw", vim.lsp.buf.document_symbol, { buffer = 0 })
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
+    vim.keymap.set("n", "<leader>af", vim.lsp.buf.code_action, { buffer = 0 })
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = 0 })
     vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
     vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
     vim.keymap.set("n", "<leader>dl", "<CMD>Telescope diagnostics<CR>", { buffer = 0 })
@@ -20,6 +25,8 @@ end
 local opts = {
     on_attach = keymaps
 }
+
+-- TODO: Add per-language settings
 
 for _, server in pairs(servers) do
     lspconfig[server].setup(opts)
