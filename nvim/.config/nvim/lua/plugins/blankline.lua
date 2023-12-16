5b6cf3f9181663f2e4e40b1dfff99577b9725ead
Jesus Sanchez <jesus.san.sev@gmail.com>
2023-12-16 15:05:07 -0500

    updated blankline

--- nvim/.config/nvim/lua/plugins/blankline.lua
+++ nvim/.config/nvim/lua/plugins/blankline.lua
@@ -1,12 +1,15 @@
 return {
     'lukas-reineke/indent-blankline.nvim',
-    event = { "BufReadPost", "BufNewFile" },
+    main = "ibl",
+    -- event = { "BufReadPost", "BufNewFile" },
     opts = {
-        filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-        char = "│",
-        space_char_blankline = " ",
-        show_current_context = true,
-        show_current_context_start = true,
-        show_end_of_line = true,
+        indent = {
+            char = "│",
+        },
+        -- filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+        -- space_char_blankline = " ",
+        -- show_current_context = true,
+        -- show_current_context_start = true,
+        -- show_end_of_line = true,
     }
 }
